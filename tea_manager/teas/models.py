d602eb3e6efa0f66a02fceb3a3ba20b04c26141c
candytuft <vishnyc3@gmail.com>
2021-03-02 18:59:16 -0500

    Adds image field to Tea Model

--- tea_manager/teas/models.py
+++ tea_manager/teas/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Tea(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
+  image = models.FileField(upload_to='images/', blank=True, null=True)
