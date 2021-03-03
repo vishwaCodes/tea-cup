a55deca01d696d74b9cd23a14c5279ad18987eb5
candytuft <vishnyc3@gmail.com>
2021-03-03 00:40:18 -0500

    Adds User and Authentication to the Server Side

--- tea_manager/teas/admin.py
+++ tea_manager/teas/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Tea
 
-admin.site.register(Tea)
+class TeaAdmin(admin.ModelAdmin):
+  list_display = ['__str__', 'user']
+  search_fields = ['user__username', 'user__email']
+  class Meta:
+    model = Tea
+
+admin.site.register(Tea, TeaAdmin)
--- tea_manager/teas/models.py
+++ tea_manager/teas/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tea(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   image = models.FileField(upload_to='images/', blank=True, null=True)
