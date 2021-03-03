167c9475312b3f25bc4d42bc26c1e08fad51b82e
candytuft <vishnyc3@gmail.com>
2021-03-02 19:57:40 -0500

    Adds full CRUD and Admin. Adds React.js App for frontend

--- tea_manager/teas/models.py
+++ tea_manager/teas/models.py
@@ -4,3 +4,6 @@ class Tea(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   image = models.FileField(upload_to='images/', blank=True, null=True)
+
+  def __str__(self):
+    return self.name
