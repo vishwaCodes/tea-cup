7537dda238cbf163d9263f5b40d90e5ec7eacaf4
candytuft <vishnyc3@gmail.com>
2021-03-02 23:13:38 -0500

    Adds both name and description properties to render from the backend side

--- frontend/tealicious/src/components/Tea.js
+++ frontend/tealicious/src/components/Tea.js
@@ -4,7 +4,8 @@ class Tea extends Component {
   render() {
     return (
       <li data-teas-index={this.props.tea.id}>
-        <span className="tea-item">{this.props.tea.name}</span>
+        <span className="tea-item">{this.props.tea.name}</span> <br/>
+        <span className="tea-item">{this.props.tea.description}</span>
       </li> 
     );
   };
