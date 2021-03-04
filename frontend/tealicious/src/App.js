ee8f6cf5eed2168fcf0dec08c61d43ecb08d6dc2
candytuft <vishnyc3@gmail.com>
2021-03-04 12:52:14 -0500

    Starts implementing Redux

--- frontend/tealicious/src/App.js
+++ frontend/tealicious/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'; 
 import routes from './config/routes';
 
-// import { Provider } from 'react-redux';
-// import store from './store';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
 
 function App() {
   return (
+    <Provider store={store}>
       <div>
         {routes}
       </div>
+    </Provider>  
   );
 }
 
--- frontend/tealicious/src/reducers/index.js
+++ frontend/tealicious/src/reducers/index.js
@@ -1,13 +1,4 @@
-// import { combineReducers } from 'redux';
-// import TeasList from '../components/TeasList';
-
-// export default combineReducers({
-//   teaslist: TeasList
-// });
-
 import { combineReducers } from 'redux';
-import auth from './auth';
 
-export default combineReducers({
-  auth
-});
+
+export default combineReducers({});
