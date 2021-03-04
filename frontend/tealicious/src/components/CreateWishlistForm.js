258f44f18a28e5e3400783eb61c56ecf5489031b
candytuft <vishnyc3@gmail.com>
2021-03-04 14:52:00 -0500

    Adds Create Wishlist Form

---
+++ frontend/tealicious/src/components/CreateWishlistForm.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class CreateWishlistForm extends Component {
+  state = {
+    wishlist: '',
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      wishlist: event.target.value,
+    });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    let wishlist = this.state.wishlist;
+    this.props.createWishlist(wishlist);
+    this.setState({
+      wishlist: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onFormSubmit} id='taskForm'>
+          <input
+            onChange={this.onInputChange}
+            type='text' id='newItemDescription'
+            placeholder='Which tea would you like to add?'
+            value={this.state.wishlist}
+          />
+          <button type='submit' id='addTask' className='btn'>Add Tea</button>
+        </form>
+      </div>
+    );
+  };
+};
+
+export default CreateWishlistForm;
