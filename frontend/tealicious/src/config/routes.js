e7691c96d457b1574ed7250bcec95fff61bd032f
candytuft <vishnyc3@gmail.com>
2021-03-04 13:38:29 -0500

    Adds some refactoring for Teas data with a new Teaslist Page

--- frontend/tealicious/src/config/routes.js
+++ frontend/tealicious/src/config/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import TeaContainer from '../containers/TeaContainer';
+import TeasPage from '../pages/TeasPage';
 import HomePage from '../pages/HomePage';
 import SignupPage from '../pages/SignupPage';
 import LoginPage from '../pages/LoginPage';
@@ -9,7 +9,7 @@ import WishlistPage from '../pages/WishlistPage';
 export default (
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/teas' component={TeaContainer} />
+      <Route path='/teas' component={TeasPage} />
       <Route path='/login' component={LoginPage} />
       <Route path='/signup' component={SignupPage} />
       <Route path='/wishlist' component={WishlistPage} />
---
+++ frontend/tealicious/src/pages/TeasPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TeaContainer from '../containers/TeaContainer';
+
+const TeasPage = () => {
+  return (
+    <div>
+      <TeaContainer />
+    </div>
+  );
+};
+
+export default TeasPage;
