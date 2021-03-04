43ac9a2044392b6a4ab4a8951b351f0a27d30e87
candytuft <vishnyc3@gmail.com>
2021-03-04 10:26:27 -0500

    Adds Login and Signup Route Paths

--- frontend/tealicious/src/config/routes.js
+++ frontend/tealicious/src/config/routes.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import TeaContainer from '../containers/TeaContainer';
 import HomePage from '../pages/HomePage';
 import TeasList from '../components/TeasList';
+import SignupPage from '../pages/SignupPage';
+import LoginPage from '../pages/LoginPage';
 
 export default (
   <Router>
@@ -10,6 +12,8 @@ export default (
       <Route exact path='/' component={HomePage} />
       <Route path='/teas' component={TeaContainer} />
       <Route path='/:teaID' component={TeasList} />
+      <Route path='/login' component={LoginPage} />
+      <Route path='/signup' component={SignupPage} />
     </Switch>
   </Router>
 );
