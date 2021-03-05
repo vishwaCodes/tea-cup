6e633c9f749440f11688c03b0885a0d6763bb4f9
candytuft <vishnyc3@gmail.com>
2021-03-05 01:48:09 -0500

    Adds images to Products for testing purrposes, Shop Page renders successfully

--- frontend/tealicious/src/components/Products/Products.jsx
+++ frontend/tealicious/src/components/Products/Products.jsx
@@ -4,8 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 
 const products = [
-  { id: 1, name: 'Herbal Tea', description: 'Herbal Tea is good for you', price: '$10'},
-  { id: 2, name: 'Black Tea', description: 'Black Tea wakes you up', price: '$7'},
+  { id: 1, name: 'Herbal Tea', description: 'Herbal Tea is good for you', price: '$10', image: 'https://i0.wp.com/cdn-prod.medicalnewstoday.com/content/images/articles/322/322624/herbal-tea-for-constipation.jpg?w=1155&h=1541'},
+  { id: 2, name: 'Black Tea', description: 'Black Tea wakes you up', price: '$7', image: 'https://cdn-prod.medicalnewstoday.com/content/images/articles/319/319646/black-tea.jpg'},
 ]
 
 const Products = () => {
