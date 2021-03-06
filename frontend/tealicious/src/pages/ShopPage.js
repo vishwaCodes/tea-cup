16e575c91304973d9832b62bb4024e8d5470963c
candytuft <vishnyc3@gmail.com>
2021-03-05 22:19:20 -0500

    Adds Cart to Shop Page, successfully fetches data from Commerce.js API

--- frontend/tealicious/src/pages/ShopPage.js
+++ frontend/tealicious/src/pages/ShopPage.js
@@ -5,6 +5,7 @@ import { Products, ShopNavbar } from '../components';
 
 const ShopPage = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -12,14 +13,27 @@ const ShopPage = () => {
     setProducts(data);
   }
 
+  const fetchCart  = async () => {
+    setCart(await commerce.cart.retrieve())
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  }
+
   useEffect(() => {
     fetchProducts();
+    fetchCart();
   }, []);
+
+  console.log(cart);
 
   return (
   <div>
     <ShopNavbar />
-    <Products products={products} />
+    <Products products={products} onAddToCart={handleAddToCart} />
   </div>
   );
 };
