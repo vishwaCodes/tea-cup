2a814fca892cf88ea99c0a5dffd2563f6a5a5cd2
candytuft <vishnyc3@gmail.com>
2021-03-05 21:02:20 -0500

    Adds Yarn Lock,  Environment Variables and implements Commerce.js

--- frontend/tealicious/src/pages/ShopPage.js
+++ frontend/tealicious/src/pages/ShopPage.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
-// import Products from '../components/Products/Products';
-// import ShopNavbar from '../components/Navbar/ShopNavbar';
-
+import React, { useState, useEffect } from 'react';
+import { commerce } from '../lib/commerce';
 import { Products, ShopNavbar } from '../components';
 
+
 const ShopPage = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log(products);
+
   return (
   <div>
     <ShopNavbar />
