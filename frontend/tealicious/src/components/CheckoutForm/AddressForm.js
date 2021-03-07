35e40c436eb748bf8c642cd464725a28756b57ac
candytuft <vishnyc3@gmail.com>
2021-03-07 08:47:10 -0500

    Successfully populates Subdivisions for all Countries on Checkout Page

--- frontend/tealicious/src/components/CheckoutForm/AddressForm.js
+++ frontend/tealicious/src/components/CheckoutForm/AddressForm.js
@@ -16,6 +16,11 @@ const AddressForm = ({ checkoutToken }) => {
   const countries = Object.entries(shippingCountries).map(([code, countryName]) => ({ id: code, label: countryName }))
   console.log('countries:', countries);
 
+  const subdivisions = Object.entries(shippingSubdivisions).map(([subCode, subdivisionName]) => ({ id: subCode, label: subdivisionName }))
+  console.log('subdivisions:', subdivisions);
+  
+
+
   const fetchShippingCountries = async (checkoutTokenId) => {
     const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
 
@@ -23,9 +28,21 @@ const AddressForm = ({ checkoutToken }) => {
     setShippingCountry(Object.keys(countries)[0]);
   }
 
+  const fetchSubdivisions = async (countryCode) => {
+    const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+
+    setShippingSubdivisions(subdivisions);
+    setShippingSubdivisions(Object.keys(subdivisions));
+  }
+
   useEffect(() => {
     fetchShippingCountries(checkoutToken.id)
   }, []);
+
+  useEffect(() => {
+    if (shippingCountry) fetchSubdivisions(shippingCountry);
+
+  }, [shippingCountry]);
 
   const methods = useForm();
 
@@ -53,16 +70,18 @@ const AddressForm = ({ checkoutToken }) => {
               </Select>
             </Grid>
 
-            {/* <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Subdivision</InputLabel>
-              <Select value={} fullWidth onChange={}>
-                <MenuItem key={} value={}>
-                  Select 
-                </MenuItem>
+              <Select value={shippingSubdivision} fullWidth onChange={(e) => setShippingSubdivision(e.target.value)}>
+                {subdivisions.map((subdivision) => (
+                  <MenuItem key={subdivision.id} value={subdivision.id}>
+                    {subdivision.label}
+                  </MenuItem>
+                ))}
               </Select>
             </Grid>
 
-            <Grid item xs={12} sm={6}>
+            {/* <Grid item xs={12} sm={6}>
               <InputLabel>Shipping Options</InputLabel>
               <Select value={} fullWidth onChange={}>
                 <MenuItem key={} value={}>
