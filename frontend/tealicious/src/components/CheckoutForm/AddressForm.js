6f283910efd5cff78f64f1b14e0c6f75ee2a9713
candytuft <vishnyc3@gmail.com>
2021-03-07 04:13:55 -0500

    Adds Checkout Form Input using React Hook Form

--- frontend/tealicious/src/components/CheckoutForm/AddressForm.js
+++ frontend/tealicious/src/components/CheckoutForm/AddressForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
+import CheckoutFormInput from './CheckoutFormInput';
 
 const AddressForm = () => {
   const methods = useForm();
@@ -9,9 +10,9 @@ const AddressForm = () => {
     <>
       <Typography variant='h6' gutterBottom>Shipping Address</Typography>
       <FormProvider {...methods}>
-        <form onSubmit={}>
+        <form onSubmit=''>
           <Grid container spacing={3}>
-            
+            <CheckoutFormInput required name='firstName' label='First Name' />
           </Grid>
         </form>
       </FormProvider>
---
+++ frontend/tealicious/src/components/CheckoutForm/CheckoutFormInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { TextField, Grid } from '@material-ui/core';
+import { Controller, useFormContext } from 'react-hook-form';
+
+
+function CheckoutFormInput({ name, label, required }) {
+  const { control } = useFormContext();
+  const isError = false;
+
+  return (
+    <Grid item xs={12} sm={6}>
+      <Controller
+        as={TextField}
+        name={name}
+        control={control}
+        label={label}
+        fullWidth
+        required={required}
+        error={isError}
+      />
+    </Grid>
+  );
+}
+
+export default CheckoutFormInput;
