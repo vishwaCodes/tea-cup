17b2211ecc9acf330df610971e6fdd2484d56be4
candytuft <vishnyc3@gmail.com>
2021-03-07 03:28:46 -0500

    Adds layout to Address Form, Adds React Hook Form

--- frontend/tealicious/src/components/CheckoutForm/AddressForm.js
+++ frontend/tealicious/src/components/CheckoutForm/AddressForm.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
 
 const AddressForm = () => {
+  const methods = useForm();
+
   return (
-    <div>
-      Address Form
-    </div>
+    <>
+      <Typography variant='h6' gutterBottom>Shipping Address</Typography>
+      <FormProvider {...methods}>
+        <form onSubmit={}>
+          <Grid container spacing={3}>
+            
+          </Grid>
+        </form>
+      </FormProvider>
+    </>
   );
 };
 
