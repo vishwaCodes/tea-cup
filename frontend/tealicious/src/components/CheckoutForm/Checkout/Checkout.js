90fd63cc22e290e3dcb301782feb8b10f92af742
candytuft <vishnyc3@gmail.com>
2021-03-07 22:51:59 -0500

    Adds Stripe and Payment Form Component

--- frontend/tealicious/src/components/CheckoutForm/Checkout/Checkout.js
+++ frontend/tealicious/src/components/CheckoutForm/Checkout/Checkout.js
@@ -20,9 +20,6 @@ const Checkout = ({ cart }) => {
       try {
         const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' });
 
-        console.log('Token:', token);
-
-
         setCheckoutToken(token);
       } catch (error) {
         
@@ -50,7 +47,7 @@ const Checkout = ({ cart }) => {
 
   const Form = () => activeStep === 0
   ? <AddressForm checkoutToken={checkoutToken} next={next} />
-  : <PaymentForm shippingData={shippingData} />
+  : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
 
   return (
     <>
--- frontend/tealicious/src/components/CheckoutForm/PaymentForm.js
+++ frontend/tealicious/src/components/CheckoutForm/PaymentForm.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+import Review from './Review';
 
 const PaymentForm = () => {
   return (
-    <div>
-      Payment Form
-    </div>
+    <>
+      <Review />
+    </>
   );
 };
 
